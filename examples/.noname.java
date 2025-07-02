3ad5c5b5d82f869ea8fab85c9358a7a155e1ddcc
Ian Riakhovskii <34051184+jardevbox@users.noreply.github.com>
2025-07-02 19:55:31 -0400

    update

--- examples/.noname.java
+++ examples/.noname.java
@@ -1,3 +1,47 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Repository: https://github.com/spring-projects/spring-boot
+*    Source File: spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/livereload/Connection.java
+*    
+*    Copyrights:
+*      copyright 2012-2021 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Source File: src/main/java/org/apache/commons/codec/binary/Base64.java
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Source File: src/main/java/org/apache/commons/codec/binary/Base64.java
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    
+*    Source File: spring-boot-cli/src/main/java/org/springframework/boot/cli/command/options/SourceOptions.java
+*    
+*    Copyrights:
+*      copyright 2012-2014 the original author or authors
+*    
+*    Licenses:
+*      Wink License
+*      SPDXId: xa0
+*    
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 private void readWebSocketFrame() throws IOException {
 		try {
 			Frame frame = Frame.read(this.inputStream);
